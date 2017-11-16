e64ea08f52f728467e6ff7187bd0ff76b775a511
Flavio Curella <flavio.curella@gmail.com>
2017-11-16 11:23:39 -0600

    Bump version: 0.0.2 → 0.0.3

--- genericclient_base/__init__.py
+++ genericclient_base/__init__.py
@@ -6,7 +6,7 @@ except ImportError:
 from . import exceptions, utils
 from .routes import DetailRoute, ListRoute
 
-_version = "0.0.2"
+_version = "0.0.3"
 __version__ = VERSION = tuple(map(int, _version.split('.')))
 
 
