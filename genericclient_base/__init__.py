fe14588f8efd6e69a53758aaf748202cb3156412
Flavio Curella <flavio.curella@gmail.com>
2018-03-28 11:56:12 -0500

    Bump version: 1.0.0 → 1.1.0

--- genericclient_base/__init__.py
+++ genericclient_base/__init__.py
@@ -7,7 +7,7 @@ from . import exceptions, utils
 from .response import ParsedResponse  # noqa
 from .routes import DetailRoute, ListRoute
 
-_version = "1.0.0"
+_version = "1.1.0"
 __version__ = VERSION = tuple(map(int, _version.split('.')))
 
 
